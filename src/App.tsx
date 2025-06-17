923e48c8d6c2b6ca506b6978d1ce772744b2b4b7
Abolfazl-Karbakhah <138566793+Abolfazl-Karbakhah@users.noreply.github.com>
2025-06-17 12:45:31 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { DrawerDialogDemo } from "@/components/drawer/drawerUI";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-2xl font-black bg-blue-400 py-2">shadcan ui in Project</h1>
+      <DrawerDialogDemo />
+    </div>
+  );
+}
+
+export default App;
